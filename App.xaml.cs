e58deff60d28c10ed7072a8142707d413e951c00
Eduado Gomez <egomezr@outlook.com>
2022-08-17 17:50:46 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿
+
+using Syncfusion.Licensing;
+
+namespace PerfectPay;
+
+public partial class App : Application
+{
+    const string KEY = "Njk5MjAxQDMyMzAyZTMyMmUzMEgzd2U2UnN5VEFtS0tLOUIwR25vTFpQTTZPS2IwZStKSkRLTXR4dVZ3cFk9";
+    public App()
+    {
+        SyncfusionLicenseProvider.RegisterLicense(KEY);
+        InitializeComponent();
+
+        MainPage = new MainPage();
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Syncfusion.Maui.Sliders;
+
+namespace PerfectPay;
+
+public partial class MainPage : ContentPage
+{
+    decimal bill;
+    int NumberaPeople = 1;
+    int tip;
+
+    public MainPage()
+    {
+        InitializeComponent();
+        TotalBillContaner.IsVisible = false;
+    }
+
+    private void Add_Remove_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void Percentage_Button_clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void Entrybill_Completed(object sender, EventArgs e)
+    {
+        if (decimal.TryParse(entrybill.Text, out var val))
+        {
+            bill = val;
+        }
+        CalculateTotlal();
+    }
+
+    private void CalculateTotlal()
+    {
+    }
+
+    private void PercentageSlider_ValueChanged(object sender, SliderValueChangedEventArgs e)
+    {
+        // if I assign the value here, I get the error
+    }
+}
+
